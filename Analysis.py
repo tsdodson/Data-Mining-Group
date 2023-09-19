7cef3d6d4a0d58c87d6c6ea0f1325478944ca081
Tyler Dodson <tsdodson@uark.edu>
2023-09-19 09:34:47 -0500

    created decision tree

--- Analysis.py
+++ Analysis.py
@@ -1,107 +1,69 @@
-import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+from IPython.display import Image
+from six import StringIO
+from sklearn.tree import export_graphviz
+import pydotplus, graphviz
+import random
+import time
 import os
-  
-# Function importing Dataset
-def importdata():
-    balance_data = pd.read_csv(
-'https://archive.ics.uci.edu/ml/machine-learning-'+
-'databases/balance-scale/balance-scale.data',
-    sep= ',', header = None)
-      
-    # Printing the dataswet shape
-    print ("Dataset Length: ", len(balance_data))
-    print ("Dataset Shape: ", balance_data.shape)
-      
-    # Printing the dataset obseravtions
-    print ("Dataset: ",balance_data.head())
-    return balance_data
-  
-# Function to split the dataset
-def splitdataset(balance_data):
-  
-    # Separating the target variable
-    X = balance_data.values[:, 1:5]
-    Y = balance_data.values[:, 0]
-  
-    # Splitting the dataset into train and test
-    X_train, X_test, y_train, y_test = train_test_split( 
-    X, Y, test_size = 0.3, random_state = 100)
-      
-    return X, Y, X_train, X_test, y_train, y_test
-      
-# Function to perform training with giniIndex.
-def train_using_gini(X_train, X_test, y_train):
-  
-    # Creating the classifier object
-    clf_gini = DecisionTreeClassifier(criterion = "gini",
-            random_state = 100,max_depth=3, min_samples_leaf=5)
-  
-    # Performing training
-    clf_gini.fit(X_train, y_train)
-    return clf_gini
-      
-# Function to perform training with entropy.
-def tarin_using_entropy(X_train, X_test, y_train):
-  
-    # Decision tree with entropy
-    clf_entropy = DecisionTreeClassifier(
-            criterion = "entropy", random_state = 100,
-            max_depth = 3, min_samples_leaf = 5)
-  
-    # Performing training
-    clf_entropy.fit(X_train, y_train)
-    return clf_entropy
-  
-  
-# Function to make predictions
-def prediction(X_test, clf_object):
-  
-    # Predicton on test with giniIndex
-    y_pred = clf_object.predict(X_test)
-    print("Predicted values:")
-    print(y_pred)
-    return y_pred
-      
-# Function to calculate accuracy
-def cal_accuracy(y_test, y_pred):
-      
-    print("Confusion Matrix: ",
-        confusion_matrix(y_test, y_pred))
-      
-    print ("Accuracy : ",
-    accuracy_score(y_test,y_pred)*100)
-      
-    print("Report : ",
-    classification_report(y_test, y_pred))
-  
-# Driver code
-def main():
-      
-    # Building Phase
-    data = importdata()
-    X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
-    clf_gini = train_using_gini(X_train, X_test, y_train)
-    clf_entropy = tarin_using_entropy(X_train, X_test, y_train)
-      
-    # Operational Phase
-    print("Results Using Gini Index:")
-      
-    # Prediction using gini
-    y_pred_gini = prediction(X_test, clf_gini)
-    cal_accuracy(y_test, y_pred_gini)
-      
-    print("Results Using Entropy:")
-    # Prediction using entropy
-    y_pred_entropy = prediction(X_test, clf_entropy)
-    cal_accuracy(y_test, y_pred_entropy)
-      
-      
-# Calling main function
-if __name__=="__main__":
-    main()
+
+
+col_names = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'salary']
+# load dataset
+data = pd.read_csv("adult.csv",header=0, names=['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'salary'])
+# data.to_csv('adult.csv', index=None)
+
+missing_values = (data==' ?').sum()
+# print(missing_values)
+
+data = data[data['workclass'] != ' ?']
+# print(data.head())
+
+categorical = data.select_dtypes(include=['object'])
+
+encoder = preprocessing.LabelEncoder()
+
+data = data[data['occupation'] != ' ?']
+data = data[data['native-country'] != ' ?']
+
+categorical = categorical.apply(encoder.fit_transform)
+# print(categorical.head())
+
+data = data.drop(categorical.columns, axis = 1)
+data = pd.concat([data, categorical], axis = 1)
+# print(data.head())
+
+# print(data.info())
+
+data['salary'] = data['salary'].astype('category')
+
+X = data.drop('salary', axis = 1)
+y = data['salary']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state = 99)
+
+tree = DecisionTreeClassifier(max_depth = 5)
+tree.fit(X_train, y_train)
+
+ypred = tree.predict(X_test)
+print(classification_report(y_test,ypred))
+print(confusion_matrix(y_test,ypred))
+print(accuracy_score(y_test,ypred))
+
+feat = list(data.columns[1:])
+# print(feat)
+
+dot_data = StringIO()
+export_graphviz(tree, out_file=dot_data,feature_names=feat, filled=True,rounded=True)
+
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png("dtree.png")
